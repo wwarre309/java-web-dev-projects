0621af39177bbf56c8de680e2bfcb8dfb220c1a7
Will <138933808+wwarre309@users.noreply.github.com>
2023-08-22 15:47:52 -0400

    Studio done

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,10 +1,30 @@
 package org.launchcode;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Area {
     public static void main(String[] args) {
         Scanner inputRadius = new Scanner(System.in);
-        System.out.print("Enter a radius: ");
-        double radius = inputRadius.nextDouble();
+        double radius = 0;
+
+        while (true) {
+            System.out.print("Enter a radius: ");
+            try {
+                radius = inputRadius.nextDouble();
+
+                if (radius < 0) {
+                    System.out.println("Error: Radius cannot be negative. Please try again.");
+                    continue;
+                }
+
+                break;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Invalid input. Please enter a valid number.");
+                inputRadius.nextLine();
+            }
+        }
 
         double area = Circle.getArea(radius);
 
