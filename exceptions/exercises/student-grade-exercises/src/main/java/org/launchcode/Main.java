db59e25b1e2138c163a5acbe212a730aca409add
Will <138933808+wwarre309@users.noreply.github.com>
2023-09-12 16:59:10 -0400

    Exercise done

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,6 +5,12 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        //Divide(10,2);
+        //Divide(10, 0);
+
+
+        // comment out Brad to check for points
+
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -12,16 +18,42 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+        for (String student : studentFiles.keySet()) {
+            String fileName = studentFiles.get(student);
+            int points = CheckFileExtension(fileName);
+            System.out.println(student + ": " + points + " points");
+        }
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static void Divide(int x, int y) {
+         try {
+             if (y == 0) {
+                 throw new ArithmeticException("Can't Divide By 0 Dummy");
+             }
+             System.out.println(x/y);
+    } catch (ArithmeticException e) {
+             e.printStackTrace();
+         }
+
+
         // Write code here!
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+    public static int CheckFileExtension(String fileName){
+        try {
+        if (fileName == null || fileName.isEmpty()) {
+        throw new Exception("File name is null or empty");
+        }
+        if (fileName.endsWith(".java")) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
         // Write code here!
     }
 }
